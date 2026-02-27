3e9704bc8503c0b2d2b54c23a60325e68d3a5bb4
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2026-02-27 21:03:00 +0100

    Add action in the main activity to create a shortcut

--- app/src/main/java/com/example/wolpowerbutton/MainActivity.java
+++ app/src/main/java/com/example/wolpowerbutton/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
 
 public class MainActivity extends Activity {
 
@@ -23,6 +26,21 @@ public class MainActivity extends Activity {
             Intent intent = new Intent(SendWolReceiver.ACTION_SEND_WOL);
             intent.setClass(this, SendWolReceiver.class);
             sendBroadcast(intent);
+        });
+
+        Button shortcutButton = findViewById(R.id.createShortcutButton);
+        shortcutButton.setOnClickListener(v -> {
+            ShortcutManager shortcutManager = getSystemService(ShortcutManager.class);
+            ShortcutInfo shortcut = new ShortcutInfo.Builder(this, "sendwol_pinned")
+                    .setShortLabel(getString(R.string.turn_on))
+                    .setLongLabel(getString(R.string.sendwol_shortcut_short_label))
+                    .setIcon(Icon.createWithResource(this, R.mipmap.ic_launcher))
+                    .setIntent(new Intent(this, SendWolShortcutActivity.class)
+                            .setAction(SendWolReceiver.ACTION_SEND_WOL))
+                    .build();
+            if (shortcutManager.isRequestPinShortcutSupported()) {
+                shortcutManager.requestPinShortcut(shortcut, null);
+            }
         });
     }
 
